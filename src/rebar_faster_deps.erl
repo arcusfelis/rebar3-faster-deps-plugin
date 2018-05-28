ff6e25263480dbcb68fcf74990085ccffe6d2e3c
Michael Uvarov <arcusfelis@gmail.com>
2018-05-28 19:05:25 +0200

    Add debug

--- src/rebar_faster_deps.erl
+++ src/rebar_faster_deps.erl
@@ -8,4 +8,8 @@
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-    {ok, rebar_state:add_resource(State, {gitcache, rebar_gitcache_resource})}.
+    Locks = rebar_state:get(State, {locks, default}, []),
+    Locks2 = Locks,
+    State2 = rebar_state:set(State, {locks, default}, Locks2),
+    io:format(user, "locks ~p", [Locks]),
+    {ok, rebar_state:add_resource(State2, {gitcache, rebar_gitcache_resource})}.
