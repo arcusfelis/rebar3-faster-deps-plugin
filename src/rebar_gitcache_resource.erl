34c947bda6c0d841ee4f10683ce4a64af483e14f
Michael Uvarov <arcusfelis@gmail.com>
2018-05-28 20:06:20 +0200

    Unzip dep zip

--- src/rebar_gitcache_resource.erl
+++ src/rebar_gitcache_resource.erl
@@ -14,14 +14,15 @@ download(Dir, Source, State) ->
     io:format(user, "Download ~p into ~p", [Source, Dir]),
     try download_untidy(Dir, untidy_dep(Source), State) of
         {ok, _} = Result ->
-            rebar_git_resource:download(Dir, untidy_dep(Source), State);
+            Result;
         Other ->
             io:format(user, "Download ~p into ~p failed. Reason ~p",
                       [Source, Dir, Other]),
             rebar_git_resource:download(Dir, untidy_dep(Source), State)
     catch Class:Reason ->
+            Stacktrace = erlang:get_stacktrace(),
             io:format(user, "Download ~p into ~p failed. Reason ~p",
-                      [Source, Dir, {Class, Reason}]),
+                      [Source, Dir, {Class, Reason, Stacktrace}]),
             rebar_git_resource:download(Dir, untidy_dep(Source), State)
     end.
 
@@ -37,16 +38,22 @@ untidy_dep({gitcache, Repo, Vsn}) ->
 
 download_untidy(Dir, {git, "git://github.com/" ++ AddrRest, {ref,GitRef}}, State) ->
     CacheDir = cache_directory(State),
-    ensure_cachedir_exist(CacheDir),
     UserRepo = remove_git_suffix(AddrRest),
+    ZipName = GitRef ++ ".zip",
+    ZipPath = filename:join([CacheDir] ++ string:tokens(UserRepo, "/") ++ [ZipName]),
+    filelib:ensure_dir(ZipPath),
     EscapedGitRef = rebar_utils:escape_chars(GitRef),
     EscapedUserRepo = rebar_utils:escape_chars(UserRepo),
-    EscapedCacheDir = rebar_utils:escape_chars(CacheDir),
-    Cmd = io_lib:format("wget -O ~ts/~ts.zip https://github.com/~ts/archive/~ts.zip",
-                                 [EscapedCacheDir, EscapedGitRef, EscapedUserRepo, EscapedGitRef]),
-    io:format(user, "Execute ~ts", [Cmd]),
-    rebar_utils:sh(Cmd, []);
-download_untidy(Dir, _, State) ->
+    EscapedZipPath = rebar_utils:escape_chars(ZipPath),
+    case file:read_file_info(ZipPath) of
+        {ok, _} ->
+            %% exist
+            ok;
+        _ ->
+            wget_from_github(EscapedZipPath, EscapedUserRepo, EscapedGitRef)
+    end,
+    unzip_cached(EscapedZipPath, Dir);
+download_untidy(_Dir, _, _State) ->
     {error, pattern_nomatch}.
 
 remove_git_suffix(AddrRest) ->
@@ -61,5 +68,15 @@ remove_git_suffix(AddrRest) ->
 cache_directory(State) ->
     filename:join([rebar_dir:global_cache_dir(rebar_state:opts(State)), "gitcache"]).
 
-ensure_cachedir_exist(CacheDir) ->
-    filelib:ensure_dir(filename:join(CacheDir, "dummy")).
+wget_from_github(EscapedZipPath, EscapedUserRepo, EscapedGitRef) ->
+    Cmd = io_lib:format("wget -O ~ts https://github.com/~ts/archive/~ts.zip",
+                                 [EscapedZipPath, EscapedUserRepo, EscapedGitRef]),
+    io:format(user, "Execute ~ts", [Cmd]),
+    rebar_utils:sh(Cmd, []).
+
+unzip_cached(EscapedZipPath, Dir) ->
+    UnzipCmd = io_lib:format("unzip ~ts", [EscapedZipPath]),
+    io:format(user, "Execute ~ts", [UnzipCmd]),
+    rebar_utils:sh(UnzipCmd, [{cd, Dir}]),
+    %% Remove one level of inclusion (i.e. zip parent directory)
+    rebar_utils:sh("mv ./*/* .", [{cd, Dir}]).
